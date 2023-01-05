bd33d3609affb0ddb2d1f9e5c987f845b2b983d6
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-05 16:28:32 -0500

    Add project files.

---
+++ NumberGuessingGame/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int winNumber = random.Next(0, 100);
+
+        }
+    }
+}
