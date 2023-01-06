95f0531fb26df453c60ff9282e34a3e9f3b94513
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-05 19:20:05 -0500

    Finish Program

--- NumberGuessingGame/Program.cs
+++ NumberGuessingGame/Program.cs
@@ -8,6 +8,31 @@
 
             int winNumber = random.Next(0, 100);
 
+            bool win = false;
+
+
+            do
+            {
+                Console.WriteLine("Guess a number between 0 and 100: ");
+                string s = Console.ReadLine();
+
+                int i = int.Parse(s);
+
+                if (i > winNumber)
+                {
+                    Console.WriteLine("Too high! Try again!");
+                }
+                else if (i < winNumber)
+                {
+                    Console.WriteLine("Too low! Try again!");
+                }
+                else if (i == winNumber)
+                {
+                    Console.WriteLine("YOU WIN!");
+                    win = true;
+                }
+
+            } while (win == false);
         }
     }
 }
